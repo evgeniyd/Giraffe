bafb3fbed43be7d32f4b1ff8c22bb30daf2dbf1a
Evgeniy Dubinin <edubinin1989@gmail.com>
2016-07-03 22:49:59 +0300

    initial commit

---
+++ Giraffe-iOS/Giraffe-iOSTests/Giraffe_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Giraffe_iOSTests.swift
+//  Giraffe-iOSTests
+//
+//  Created by Evgen Dubinin on 7/3/16.
+//  Copyright © 2016 Yevhen Dubinin. All rights reserved.
+//
+
+import XCTest
+@testable import Giraffe_iOS
+
+class Giraffe_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ GiraffeKit/GiraffeKit/GiraffeKit.h
@@ -0,0 +1,19 @@
+//
+//  GiraffeKit.h
+//  GiraffeKit
+//
+//  Created by Evgen Dubinin on 7/3/16.
+//  Copyright © 2016 Yevhen Dubinin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GiraffeKit.
+FOUNDATION_EXPORT double GiraffeKitVersionNumber;
+
+//! Project version string for GiraffeKit.
+FOUNDATION_EXPORT const unsigned char GiraffeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GiraffeKit/PublicHeader.h>
+
+
