fb935dc4e5d1064a74f98c33182230908e9e4399
Evgeniy Dubinin <edubinin1989@gmail.com>
2016-07-04 02:42:11 +0300

    using FLAnimatedImage instead of Gifu and it seems to work way more better

--- Giraffe-iOS/Giraffe-iOS/ViewController.swift
+++ Giraffe-iOS/Giraffe-iOS/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import FLAnimatedImage
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.blueColor()
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+       
+        // remote
+        let image = FLAnimatedImage.init(animatedGIFData: NSData(contentsOfURL: NSURL(string: "https://media1.giphy.com/media/l46CpUy7GwBmjP8QM/200.gif")!))
+        let imageView = FLAnimatedImageView()
+        imageView.animatedImage = image
+        imageView.frame = CGRect(origin: CGPoint(x:0.0, y: 0.0), size: CGSize(width:  100.0, height: 100.0) )
+        self.view.addSubview(imageView)
+        
+        // local
+        let pathForFile = NSBundle.mainBundle().pathForResource("02", ofType: "gif")
+        let url = NSURL.fileURLWithPath(pathForFile!)
+        let data = NSData(contentsOfURL: url)
+        let imageLocal = FLAnimatedImage.init(animatedGIFData: data)
+        let imageViewLocal = FLAnimatedImageView()
+        imageViewLocal.animatedImage = imageLocal
+        imageViewLocal.frame = CGRect(origin: CGPoint(x:100.0, y: 0.0), size: CGSize(width:  100.0, height: 100.0) )
+        self.view.addSubview(imageViewLocal)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
