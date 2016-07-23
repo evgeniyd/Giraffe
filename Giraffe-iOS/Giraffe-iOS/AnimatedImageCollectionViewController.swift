6d52d833391afd82b8f2562472bcebf77b6208e1
Evgeniy Dubinin <edubinin1989@gmail.com>
2016-07-24 00:24:00 +0300

    Styling collectionview cells background

--- Giraffe-iOS/Giraffe-iOS/AnimatedImageCollectionViewController.swift
+++ Giraffe-iOS/Giraffe-iOS/AnimatedImageCollectionViewController.swift
@@ -12,6 +12,7 @@ import GiraffeKit
 
 final class AnimatedImageCollectionViewController: UICollectionViewController {
     private let animatedImageCellIdentifier = "AnimatedImageCellIdentifier"
+    private var flipBackgroundPatternCounter: Int = 0
     
     let rac_items = MutableProperty<[Item]>([])
     
@@ -32,6 +33,7 @@ final class AnimatedImageCollectionViewController: UICollectionViewController {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(animatedImageCellIdentifier, forIndexPath: indexPath)
         let animatedImageCell = cell as! AnimatedImageCell // check agains conforming to a protocol, not a concrete type
         animatedImageCell.bind(viewModel: self.viewModelFor(indexPath))
+        flipBackgroundColorFor(cell: animatedImageCell, indexPath: indexPath)
         return animatedImageCell
     }
     
@@ -57,5 +59,39 @@ final class AnimatedImageCollectionViewController: UICollectionViewController {
         let model = self.rac_items.value[row]
         let viewModel = AnimatedImageViewModel(model: model)
         return viewModel
+    }
+    
+    // MARK: - Styling -
+    
+    private func flipBackgroundColorFor(cell cell: UICollectionViewCell, indexPath: NSIndexPath) {
+        
+        // orange(1) yellow(2)
+        // yellow(3) orange(4)
+        // orange(5) yellow(6)
+        // yelllow(7) orange(8)
+        
+        var shouldFlip = false
+        
+        flipBackgroundPatternCounter += 1
+        if flipBackgroundPatternCounter > 4 {
+            flipBackgroundPatternCounter = 1
+        }
+        
+        switch flipBackgroundPatternCounter {
+        case 1, 2:
+            shouldFlip = true
+        case 3, 4:
+            shouldFlip = false
+        default:
+            shouldFlip = false
+        }
+        
+        let row = indexPath.row
+        if row % 2 == 0 {
+            cell.contentView.backgroundColor = shouldFlip ? UIColor.giraffeOrange() : UIColor.giraffeYellow()
+        }
+        else {
+            cell.contentView.backgroundColor = shouldFlip ? UIColor.giraffeYellow() : UIColor.giraffeOrange()
+        }
     }
 }
